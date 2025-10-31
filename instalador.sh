18c3daf05bc033fdcfdb925cbeaaccf52b2539ed
Daniel Vargas <83847722+sr-script@users.noreply.github.com>
2025-10-30 20:52:03 -0600

    Add installation script for 'caca'
    Script to install 'caca' and set permissions.

---
+++ instalador.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# instalador para caca
+
+echo "Instalando caca... ... ... ..."
+
+echo "..."
+echo " "
+echo "..."
+echo " "
+echo " "
+echo " "
+echo "..."
+echo " "
+
+
+sudo cp caca /usr/local/bin/
+sudo chmod +x /usr/local/bin/caca
+
+echo "Ya casi queda"
+echo " "
+echo "... "
+echo "Instalación completa. Puedes ejecutar 'caca' desde cualquier lugar."
